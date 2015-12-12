cb331d216bec26be323626e5b63c4375f0cb2ec0
dumlaoj <jordanobena@gmail.com>
2015-12-11 20:52:41 -1000

    created images for textbooks

--- app/server/textbooks.js
+++ app/server/textbooks.js
@@ -1,77 +1,90 @@
 Textbooks.insert({
   title: "Java Concepts: Compatible with Java 5, 6, and 7",
   course: "111",
-  isbn: "9780470509470"
+  isbn: "9780470509470",
+  img: "http://ecx.images-amazon.com/images/I/51vVZkg4MoL._SX396_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Discreet Mathematics and it's applications",
   course: "141",
-  isbn: "9780073383095"
+  isbn: "9780073383095",
+  img: "http://ecx.images-amazon.com/images/I/51b1OWPD1YL._SX403_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Data Structures; Abstraction and Design Using Java",
   course: "211",
-  isbn: "9780470128701"
+  isbn: "9780470128701",
+  img: "http://ecx.images-amazon.com/images/I/51oJG0RYKmL._SX372_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "C++ Primer Plus",
   course: "212",
-  isbn: "9780321776402"
+  isbn: "9780321776402",
+  img: "http://ecx.images-amazon.com/images/I/51hYz21HbAL._SX388_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "The C Programming Language",
   course: "212",
-  isbn: "9780131103627"
+  isbn: "9780131103627",
+  img: "http://ecx.images-amazon.com/images/I/41qX6YdIJ7L._SX379_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Programming Language Pragmatics",
   course: "313",
-  isbn: "0123745144"
+  isbn: "0123745144",
+  img: "http://ecx.images-amazon.com/images/I/51TvuWQBKnL._SX403_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Land of Lisp: Learn to Program in Lisp, One Game at a Time",
   course: "313",
-  isbn: "978-1-59327-281-4"
+  isbn: "978-1-59327-281-4",
+  img: "http://ecx.images-amazon.com/images/I/518WFfcKCpL._SX376_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Database Systems: the Complete Book",
   course: "321",
-  isbn: "9780131873254"
+  isbn: "9780131873254",
+  img: "http://ecx.images-amazon.com/images/I/51JtltOJPVL._SX353_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Digital Logic Design: a Rigorous Approach",
   course: "332",
-  isbn: "9781107027534"
+    isbn: "9781107027534",
+  img: "9781107027534"
 });
 
 Textbooks.insert({
   title: "Operating System Concepts",
   course: "332",
-  isbn: "9781118063330"
+  isbn: "9781118063330",
+  img: "http://ecx.images-amazon.com/images/I/51fAaJDGv-L._SX346_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Ethics for the Information Age",
   course: "390",
-  isbn: "9780132855532"
+  isbn: "9780132855532",
+  img: "http://ecx.images-amazon.com/images/I/51vNDL0XonL._SX396_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Presenting to Win: the Art of Telling Your Story",
   course: "462",
-  isbn: "9780137144174"
+  isbn: "9780137144174",
+  img: "http://ecx.images-amazon.com/images/I/51rD3vJmgiL._SX337_BO1,204,203,200_.jpg"
 });
 
 Textbooks.insert({
   title: "Artificial Intelligence for Games",
   course: "462",
-  isbn: "9780123747310"
+  isbn: "9780123747310",
+  img: "http://ecx.images-amazon.com/images/I/41UAR9-mTzL._SX390_BO1,204,203,200_.jpg"
 });
